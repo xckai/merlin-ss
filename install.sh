2381872edb5a0b00ff2309ce74b81f108ea8a301
xu kai <kai.xu01@sap.com>
2018-09-15 13:45:14 +0800

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 mkdir shadowsock
 d=$(pwd)
+cd "$d/shadowsock"
 curl -o "$d/shadowsock/update.sh" https://raw.githubusercontent.com/xckai/merlin-ss/master/update.sh
+chmod +x "$d/shadowsock/update.sh"
 echo "$d/shadowsock/update.sh"
 sh -c "$d/shadowsock/update.sh"
--- update.sh
+++ update.sh
@@ -3,5 +3,6 @@ d=$(pwd)
 curl -o "$d/file.list" https://raw.githubusercontent.com/xckai/merlin-ss/master/file.list
 for file in $(cat "$d/file.list"); do
     curl -o "$d/$file" "https://raw.githubusercontent.com/xckai/merlin-ss/master/$file"
+    chmod +x "$d/$file"
 done
 
