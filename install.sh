4990a91b1794744d597c48b45c10682da30d38a0
xu kai <kai.xu01@sap.com>
2018-09-15 13:27:20 +0800

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
+#! /bin/sh
 mkdir shadowsock
 cd ./shadowsock
-curl -o https://raw.githubusercontent.com/xckai/merlin-ss/master/update.sh
+curl -o update.sh https://raw.githubusercontent.com/xckai/merlin-ss/master/update.sh
 sh -c ./update.sh
