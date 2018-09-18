4a2e09bd4496466095295ea4c47284c9fd8e78b7
xu kai <kai.xu01@sap.com>
2018-09-18 17:33:03 +0800

    add direct domain

--- test.sh
+++ test.sh
@@ -1,4 +1,13 @@
 d=$(pwd)
-for host in $(cat "$d/custom_proxy.txt"); do
-        echo "server=/$host/8.8.8.8" >>"$d/custome"
+for host in '1' '2' '3'; do
+        find=false
+        for b in '2' '4';do 
+                if [ $host = $b ];then
+                        find=true
+                        break
+                fi
+        done
+        if [ $find = false ];then
+                echo $host
+        fi
 done
--- update.sh
+++ update.sh
@@ -22,29 +22,46 @@ update_gfw(){
     rm "$d/gfwlist.txt"
     curl -o "$d/gfwlist.txt" "https://cokebar.github.io/gfwlist2dnsmasq/gfwlist_domain.txt"
     rm "$d/gfwlist_gfw.list"
-    for host in $(cat "$d/gfwlist.txt"); do
-        echo "server=/$host/$remote_dns" >> "$d/gfwlist_gfw.list"
-        echo "ipset=/$host/PROXY_DST" >> "$d/gfwlist_gfw.list"
-    done
     rm "$d/gfwlist_china.list"
     for host in $(cat "$d/gfwlist.txt"); do
-        echo "server=/$host/$remote_dns" >> "$d/gfwlist_china.list"
-    done
-    
+        if [ -f "$d/direct.domain"];
+        then
+            find=false
+            for direct in $(cat "$d/direct.domain");do 
+                    if [ $host = $direct ];then
+                            find=true
+                            break
+                    fi
+            done
+            if [ $find = false ];then
+                echo "server=/$host/$remote_dns" >> "$d/gfwlist_gfw.list"
+                echo "ipset=/$host/PROXY_DST" >> "$d/gfwlist_gfw.list"
+                echo "server=/$host/$remote_dns" >> "$d/gfwlist_china.list"
+            fi
+        else
+            echo "server=/$host/$remote_dns" >> "$d/gfwlist_gfw.list"
+            echo "ipset=/$host/PROXY_DST" >> "$d/gfwlist_gfw.list"
+            echo "server=/$host/$remote_dns" >> "$d/gfwlist_china.list"
+        fi
+    done    
 }
 update_custom(){
-    for file in 'dst2direct.ip' 'dst2proxy.ip' 'proxy.domain'; do
+    for file in 'dst2direct.ip' 'dst2proxy.ip' 'proxy.domain' 'direct.domain'; do
         curl -o "$d/$file" "https://raw.githubusercontent.com/xckai/merlin-ss/master/$file"
     done
     rm "$d/custom_proxy_gfw.list"
+    rm "$d/custom_proxy_china.list"
     for host in $(cat "$d/proxy.domain"); do
         echo "server=/$host/$remote_dns" >> "$d/custom_proxy_gfw.list"
         echo "ipset=/$host/PROXY_DST" >> "$d/custom_proxy_gfw.list"
-    done
-    rm "$d/custom_proxy_china.list"
-    for host in $(cat "$d/proxy.domain"); do
         echo "server=/$host/$remote_dns" >> "$d/custom_proxy_china.list"
     done
+    for host in $(cat "$d/direct.domain"); do
+        echo "ipset=/$host/DIRECT_DST" >> "$d/custom_proxy_gfw.list"
+        echo "ipset=/$host/DIRECT_DST" >> "$d/custom_proxy_china.list"
+    done
+   
+   
 }
 start_ss(){
     "$d/ss.sh" gfwmode
@@ -68,9 +85,9 @@ case "$1" in
             ;;
         all)
             update_sh
+            update_custom
             update_chinaip
             update_gfw
-            update_custom
             ;;
         *)
             echo $"Usage: $0 {ss|adblock|gfw|chinaip|all}"
